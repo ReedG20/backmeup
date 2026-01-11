a11a2987b15cf17970342139c382d13c9bf4f09e
ReedG20 <reedgrenager@gmail.com>
2026-01-10 22:59:33 -0800

    cp

--- app/(sessions)/_layout.tsx
+++ app/(sessions)/_layout.tsx
@@ -1,15 +1,6 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function SessionsLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: '#3A20E3' },
-        headerTintColor: '#fff',
-        headerBackVisible: true,
-        contentStyle: { backgroundColor: '#3A20E3' },
-      }}
-    />
-  );
+  return <Slot />;
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,12 +26,18 @@ export default function Layout() {
   return (
     <Stack
       screenOptions={{
-        headerShown: false,
         contentStyle: { backgroundColor: '#3A20E3' },
       }}
     >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(sessions)" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(sessions)/[id]"
+        options={{
+          headerShown: true,
+          headerStyle: { backgroundColor: '#3A20E3' },
+          headerTintColor: '#fff',
+        }}
+      />
     </Stack>
   );
 }
